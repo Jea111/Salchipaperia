c4fed9f192612f73f37f28f2b9e8a1dfad16f427
jea111 <laliendra42@gmail.com>
2025-08-29 12:31:26 -0500

    Salsas y Papas

--- restaurante_app/urls.py
+++ restaurante_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .  import views
 from django.conf.urls.static import static
-
+from django.conf import settings
 
 urlpatterns = [
     path('', views.users,name='login'),
     path('inicio/', views.inicio,name='inicio'),
     path('procesar-pedido/', views.procesar_pedido, name='procesar_pedido'),
+
+
 ]
-
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
