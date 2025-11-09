96688ee990ef8e90bfac99357e13ade02e31f0cb
jea111 <laliendra42@gmail.com>
2025-11-08 23:19:54 -0500

    relacionar ventas y usuario

--- restaurante_app/urls.py
+++ restaurante_app/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
-from .  import views
+from . import views
 
 urlpatterns = [
-    path('', views.users,name='login'),
-    path('inicio/', views.inicio,name='inicio'),
-    # path('pedidos/', views.pedidosRealizados, name='pedidoRealizados'),
-
+    path('', views.inicio, name='inicio'),
+    path('pedidos/', views.pedidosUser, name='pedidos'),
 ]
